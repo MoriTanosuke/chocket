a3befd0460846a1343a7bdbba9bb146aa05945aa
Carsten <carsten@kopis.de>
2012-07-29 11:23:21 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+var app = require('http').createServer(handler)
+  , io = require('socket.io').listen(app)
+  , fs = require('fs')
+
+app.listen(8888);
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+var users = new Array();
+
+var chat = io.of('/chat').on('connection', function (socket) {
+  socket.on('login', function (data) {
+    console.log('User ' + data['username'] + ' joined');
+    socket.set('username', data['username'], function() {
+      socket.broadcast.emit('msg', {msg: 'User ' + data['username'] + ' joined'});
+      users.push(data['username']);
+    });
+    socket.emit('ready', {users: users})
+  });
+  socket.on('disconnect', function() {
+    socket.get('username', function(err, username) {
+      for(i in users) {
+        if(users[i] == username) {
+          users.splice(i, 1);
+        }
+      }
+      console.log('User ' + username + ' left');
+    });
+    socket.broadcast.emit('logout', 'User left');
+  });
+
+  socket.on('msg', function(data) {
+    socket.get('username', function(err, username) {
+      socket.broadcast.emit('msg', {msg: username + ': ' + data['msg']});
+    });
+    socket.emit('msg', {msg: 'You said: ' + data['msg']});
+  });
+});
+
