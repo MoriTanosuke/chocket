fd81461f57defaddaa4c91a5f88b7187c14721c8
unknown <philip@philip-laptop.(none)>
2012-08-06 22:05:11 +0100

    a few changes to the style and added resize method so scroll can be done by css

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function handler (req, res) {
       return res.end('Error loading index.html');
     }
 
-    res.writeHead(200);
+    res.writeHead(200, {"Content-Type": "text/html"});
     res.end(data);
   });
 }
