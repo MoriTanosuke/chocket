7f6362aed0219067729ba70eea09648483b241a8
Carsten <carsten@kopis.de>
2019-03-26 10:45:56 +0100

    Add content-type to HTML response

--- app.js
+++ app.js
@@ -44,7 +44,9 @@ function handler (req, res) {
         return res.end('Error loading index.html');
       }
   
-      res.writeHead(200);
+      res.writeHead(200, {
+        'Content-Type': 'text/html; charset=utf-8'
+      });
       res.end(data);
     });
   }
